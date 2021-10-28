146b4aa2961b656889b52beaf2ef5b992e8260b0
Long-Pro <nvl1682@gmail.com>
2021-10-28 14:49:24 +0700

    completed 10 module congig...

--- src/app/_lib/note.js
+++
@@ -1,8 +0,0 @@
-// Danh mục sp (sửa sp bị ngược)                 - done
-// Ma bao chưa làm                               - done
-// kh/đơn vị đống bao mới có giao diện           - done
-// đanh mục thiết bị(lap data, chưa test)
-
-
-// Code sạch đẹp(PAcking unit 1 trường---bill,job,reason 2 trường)
-// 
