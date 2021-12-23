05381faccaccd681e91a4d0242a4d58ac748d722
iron2014hieu <hieuttpk00808@gmail.com>
2021-12-23 15:51:58 +0700

    ud 23/12

--- src/app/core/services/excel.service.ts
+++ src/app/core/services/excel.service.ts
@@ -103,8 +103,8 @@ export class ExcelService {
       topLeftCell: { row: 3, column: 1 },
       customizeCell: ({ pivotCell, excelCell }) => {
         if (this.isDataCell(pivotCell) || this.isTotalCell(pivotCell)) {
-          const appearance: any = this.getConditionalAppearance(pivotCell);
-          Object.assign(excelCell, this.getExcelCellFormat(appearance));
+          // const appearance: any = this.getConditionalAppearance(pivotCell);
+          // Object.assign(excelCell, this.getExcelCellFormat(appearance));
         }
 
         const borderStyle = { style: 'thin', color: { argb: 'FF7E7E7E' } };
@@ -123,7 +123,8 @@ export class ExcelService {
       headerRow.getCell(1).value = heading || 'Báo cáo vận chuyển';
       headerRow.getCell(1).alignment = {
         horizontal: 'center',
-        vertical: 'middle'
+        vertical: 'middle',
+        wrapText: true
       };
       headerRow.getCell(1).font = {
         color: { argb: '000000' },
@@ -181,8 +182,8 @@ export class ExcelService {
   }): void {
     cell.area = area;
     if (this.isDataCell(cell) || this.isTotalCell(cell)) {
-      const appearance: any = this.getConditionalAppearance(cell);
-      Object.assign(cellElement.style, this.getCssStyles(appearance));
+      // const appearance: any = this.getConditionalAppearance(cell);
+      // Object.assign(cellElement.style, this.getCssStyles(appearance));
     }
   }
 
@@ -227,18 +228,18 @@ export class ExcelService {
     };
   }
 
-  getConditionalAppearance(cell: any) {
-    if (this.isTotalCell(cell)) {
-      return { fill: 'F2F2F2', font: '3F3F3F', bold: true };
-    }
-    const { value } = cell;
-    if (value < 20000) {
-      return { font: '9C0006', fill: 'FFC7CE' };
-    }
-    if (value > 50000) {
-      return { font: '006100', fill: 'C6EFCE' };
-    }
+  // getConditionalAppearance(cell: any) {
+  //   if (this.isTotalCell(cell)) {
+  //     return { fill: 'F2F2F2', font: '3F3F3F', bold: true };
+  //   }
+  //   const { value } = cell;
+  //   if (value < 20000) {
+  //     return { font: '9C0006', fill: 'FFC7CE' };
+  //   }
+  //   if (value > 50000) {
+  //     return { font: '006100', fill: 'C6EFCE' };
+  //   }
 
-    return { font: '9C6500', fill: 'FFEB9C' };
-  }
+  //   return { font: '9C6500', fill: 'FFEB9C' };
+  // }
 }
--- src/app/shipping-unit/pages/model.ts
+++ src/app/shipping-unit/pages/model.ts
@@ -1,24 +1,73 @@
+import { ShiftDetailInfo } from 'src/app/core/models/model.pb';
+
+export class Appointment {
+  id: number;
+  text: string;
+  shift: number;
+  startDate: Date;
+  endDate: Date;
+  description: string;
+  shiftDetail: ShiftDetail[];
+  constructor(data: ShiftDetailInfo) {
+    // this.text = data.nameShift
+    this.text = data.nameShift + ' ' + data.nameCreatedPerson;
+    this.startDate = new Date(data.date);
+    this.endDate = new Date(data.date);
+    this.shift = parseInt(
+      data.nameShift
+        .toLowerCase()
+        .replace('ca', '')
+        .trim()
+    );
+  }
+}
+
+export class Resource {
+  text: string;
+  id: number;
+  color: string;
+}
+export class ShiftDetail {
+  id: number;
+  option: string;
+  type: string;
+  product: string;
+  productRange: number;
+  packaging: string;
+  lot: number;
+  unit: string;
+  wareHouse: string;
+  machines_packaging?: MachinesPackaging | '' | any;
+}
+export class MachinesPackaging {
+  machine_a: number;
+  machine_b: number;
+  machine_c: number;
+  machine_d: number;
+  machine_e: number;
+}
+
 export class ItemModel {
-    name: string = '';
-    index: number = 0;
-    action: string = '';
+  name: string = '';
+  index: number = 0;
+  action: string = '';
 }
 export interface ListShift {
-    shift : number;
-    name : string;
+  shift: number;
+  name: string;
 }
 export interface DataTableMinute {
-    id: number
-    product : string;
-    type : number;
-    packaging : string;
-    lot : number;
-    seri : number;
-    quantity: number;
-    user: string;
-    shift:number;
-    startDate : Date;
-    quantityPac : number;
-    mass : number;
-    wareHouse: string;
+  id: number;
+  product: string;
+  type: number;
+  packaging: string;
+  lot: number;
+  seri: number;
+  quantity: number;
+  user: string;
+  shift: number;
+  startDate: Date;
+  quantityPac: number;
+  mass: number;
+  wareHouse: string;
 }
