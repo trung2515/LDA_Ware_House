1e77e35d632711c624cef2a95e38470d041fdee3
trung2515 <82370463+trung2515@users.noreply.github.com>
2022-03-16 09:55:02 +0700

    adminhd layout done

--- src/app/admin/layouts/configuration-layout/configuration-layout.component.ts
+++ src/app/admin/layouts/configuration-layout/configuration-layout.component.ts
@@ -14,17 +14,17 @@ export class ConfigurationLayoutComponent implements OnInit {
   
   routing:any=[
     {link:'/configuration/product-category',name:'Danh mục sản phẩm'},
-    {link:'/configuration/warehouse-category',name:'Danh mục kho'},
     {link:'/configuration/device-category',name:'Danh mục thiết bị'},
-    {link:'/configuration/type-bag',name:'Loại bao'},
+    {link:'/configuration/type-bag',name:'Danh mục loại bao'},
+    {link:'/configuration/packet-unit',name:'Danh mục đơn vị đóng'},
+    {link:'/configuration/warehouse-category',name:'Danh mục kho'},
+    {link:'/configuration/reason',name:'Danh mục mã lỗi'},
+    {link:'/configuration/line',name:'Danh mục line'},
     // {link:'/configuration/id-bag',name:'Mã bao'},
     // {link:'/configuration/partner',name:'Khách hàng/Đơn vị đóng bao'},
-    {link:'/configuration/packet-unit',name:'Đơn vị đóng'},
     // {link:'/configuration/bill',name:'Các loại phiếu'},
     // {link:'/configuration/job',name:'Công việc'},
-    {link:'/configuration/reason',name:'Nguyên nhân'},
     // {link:'/configuration/minutes',name:'Biên bản'},
-    {link:'/configuration/line',name:'Line'},
   ]
 
 }
